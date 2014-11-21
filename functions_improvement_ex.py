0915d8394fe36ab8c2a690ba32df3685629153f3
Hugo Bahman <41408@longroad.ac.uk>
2014-11-21 14:30:27 +0000

    Times Tables Functions

--- functions_improvement_ex.py
+++ functions_improvement_ex.py
@@ -1,22 +1,54 @@
 # functions improvement exercise
 # times-table tester
+
+
 import random
 
+def get_times_tables():
+    print("Times-table tester")
+    print()
+    testTable = int(input("Which times-table do you want to be tested on? ")
+    return timesTable
+
+def generate_questions(timesTable):
+    for questions in range(1,):
+        Num1 = get_times_tables()
+        Num2 = random.randrange(2,13)
+        Ans = Num1 * Num2
+        return Num1, Num2, answer
+
+def ask_questions(Num1, Num2, answer):
+        generate_questions(timesTables)
+        userAnswer = int(input((Num1) + 'x' + str(Num2) + ' = ? ')
+        if userAnswer == answer
+            print("Well done, you got it right!")
+        else:
+            print("That's wrong. The correct answer is {0}".format(answer))
+
+def times_tables_tester():
+        ask_questions(Num1, Num2, answer)
+
+times_tables_tester()
+    
+ 
+
+
+
 # main program
-print("Times-table tester")
-print()
-testTable = input("Which times-table do you want to be tested on? ")
-testTable = int(testTable)
-for questions in range(1,6):
-    Num1 = testTable
-    Num2 = random.randrange(2,13)
-    Ans = Num1 * Num2
-    UserAnswer = input(str(Num1) + ' x ' + str(Num2) + ' = ? ')
-    UserAnswer = int(UserAnswer)
-    if UserAnswer == Ans:
-        print('Well done, you got the correct answer!')
-        print()
-    else:
-        print('Sorry, you got the answer wrong. The correct answer is',Ans)
-        print()
-              
+#print("Times-table tester")
+#print()
+#testTable = input("Which times-table do you want to be tested on? ")
+#testTable = int(testTable)
+#for questions in range(1,6):
+#   Num1 = testTable
+#    Num2 = random.randrange(2,13)
+ #   Ans = Num1 * Num2
+    #UserAnswer = input(str(Num1) + ' x ' + str(Num2) + ' = ? ')
+  #  UserAnswer = int(UserAnswer)
+    #if UserAnswer == Ans:
+#        print('Well done, you got the correct answer!')
+ #       print()
+  #  else:
+   #     print('Sorry, you got the answer wrong. The correct answer is',Ans)
+    #    print()
+    
