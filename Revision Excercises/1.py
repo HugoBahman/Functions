94bc1d4acbc47027eef05197d04dca57e11990ed
Hugo Bahman <41408@longroad.ac.uk>
2014-11-25 13:01:37 +0000

    Revision Excercise 1

---
+++ Revision Excercises/1.py
@@ -0,0 +1,10 @@
+#Hugo
+#Revision Excercise 1
+
+def OutputSymbols():
+    number = int(input("Please enter a whole number: "))
+    symbol = input("Please enter a symbol: ")
+    for count in range(1):
+        print(symbol*number)
+
+blah = OutputSymbols()
