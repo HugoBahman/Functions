1b7b20bc33d17643358302cb27a0d0642b010a01
Hugo Bahman <41408@longroad.ac.uk>
2014-11-28 14:30:42 +0000

    Incomplete

---
+++ Revision Excercises/3.py
@@ -0,0 +1,8 @@
+def sort(no_1, no_2):
+    if no_1 > no_2:
+        return no_2, no_1
+
+no_1 = int(input("Enter first number: "))
+no_2 = int(input("Enter second number: "))
+
+print(sort(no_1. no_2))
