2a66b8b3433a9b65bb61dbce05a6b4a42d81ae31
Hugo Bahman <41408@longroad.ac.uk>
2014-12-05 14:31:31 +0000

    New Folder

---
+++ Stretch Challenge/1.py
@@ -0,0 +1,12 @@
+#Hugo
+#Stretch & Challenge
+
+def get_information():
+    choice = input("Are you converting from imperial to metric or metric to imperial (M-I/I-M): ")
+    if choice == "I-M":
+        feet = float(input("How many feet: "))
+        inches = float(input("How many inches: "))
+        
+
+def conversion():
+    
